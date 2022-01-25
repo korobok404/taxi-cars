d8858f88db9d1e1d8e0bab03e4a38cfe81e703cb
Korobok404 <febram404@gmail.com>
2022-01-25 20:49:28 +0700

    Add gracefully shutdown and server configs

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -17,7 +22,39 @@ func main() {
 	router.PUT("/cars/:id", updateCarById)
 	router.DELETE("/cars/:id", deleteCarById)
 
-	router.Run(":8080")
+	// Server settings
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	// Start server
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Println("Error starting server:", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Wait for signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Gracefully shutting down.")
+
+	// Gracefully shutdown the server
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Error shutting down:", err)
+	}
+
 }
 
 type Car struct {
